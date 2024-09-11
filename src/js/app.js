98b7dd6dcd64d0783998b1c5498963b3814040a7
Your Name <you@example.com>
2024-09-11 20:28:37 +0500

    initial vommit

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,39 @@
 // TODO: write code here
 
 console.log('app.js bundled');
+
+const form = document.querySelector(".form");
+
+const errors = {
+login: {
+   valueMissing: "Представьтесь пожалуйста",
+},
+email: {
+   valueMissing: "Нам потребуется элетропочта...",
+   typeMismatch: "А это точона электропоята?", 
+}
+};
+form.addEventListener("submit", (e) => {
+e.preventDefault();
+ if (form.checkValidity()) {
+    console.log("valid");
+ } else {
+    console.log("invalid");
+ }
+
+ const elements = form.elements;
+ elements.some((el) => {
+   console.dir(el);
+   Object.keys(ValidityState.prototype).forEach((key) => {
+if(!el.name) return;
+
+   if(el.validity[key]) {
+  console.log(key);
+  console.log(el.errors[el.name][key]);
+   }
+   })
+ })
+console.log("submit");
+
+})
+
