a0cc304e3d18aa4ec214a6f1ebcfc59ca77a1a78
Your Name <you@example.com>
2024-09-23 12:01:41 +0500

    init

---
+++ src/js/tooltip.js
@@ -0,0 +1,10 @@
+export class Tooltip {
+    constructor() {
+        this._tooltips = [];
+    }
+
+showTooltip(message, element) {
+    const tooltipElement = document.createElement('div');
+    tooltipElement.classList.add('')
+}
+}
